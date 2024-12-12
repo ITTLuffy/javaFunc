593c24238f8b17563f991461660f04635e4abde1
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-12 14:08:14 +0100

    Fix: add forgotten Scanner

--- methodsIntro/src/App.java
+++ methodsIntro/src/App.java
@@ -4,45 +4,45 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        Scanner in = new Scanner(System.in);
+        try (Scanner in = new Scanner(System.in)) {
+            boolean sentinella;
 
-        boolean sentinella;
+            sentinella = true;
 
-        sentinella = true;
+            while (sentinella) {
+                System.out.println("Quale operazione vuoi fare?");
+                System.out.println("Premi 1 per addizione, premi 2 per sottrazione, premi 3 per divisione, premi 4 per moltiplicazione e premi 0 per uscire");
 
-        while (sentinella) {
-            System.out.println("Quale operazione vuoi fare?");
-            System.out.println("Premi 1 per addizione, premi 2 per sottrazione, premi 3 per divisione, premi 4 per moltiplicazione e premi 0 per uscire");
+                int nInserito = in.nextInt();
 
-            int nInserito = in.nextInt();
+                switch (nInserito) {
+                    case 1 -> {
+                        System.out.println("Inserisci due numeri");
+                        int somma = addizione(in.nextInt(), in.nextInt());
+                        System.out.println(somma);
+                    }
+                    case 2 -> {
+                        System.out.println("Inserisci due numeri");
+                        int somma2 = sottrazione(in.nextInt(), in.nextInt());
+                        System.out.println(somma2);
+                    }
+                    case 3 -> {
+                        System.out.println("Inserisci due numeri");
+                        float somma3 = divisione(in.nextFloat(), in.nextInt());
+                        System.out.println(somma3);
+                    }
+                    case 4 -> {
+                        System.out.println("Inserisci due numeri");
+                        int somma4 = moltiplicazione(in.nextInt(), in.nextInt());
+                        System.out.println(somma4);
+                    }
+                    case 0 -> {
+                        System.err.println("Uscita");
+                        sentinella = false;
+                    }
+                }
 
-            switch (nInserito) {
-                case 1 -> {
-                    System.out.println("Inserisci due numeri");
-                    int somma = addizione(in.nextInt(), in.nextInt());
-                    System.out.println(somma);
-                }
-                case 2 -> {
-                    System.out.println("Inserisci due numeri");
-                    int somma2 = sottrazione(in.nextInt(), in.nextInt());
-                    System.out.println(somma2);
-                }
-                case 3 -> {
-                    System.out.println("Inserisci due numeri");
-                    float somma3 = divisione(in.nextFloat(), in.nextInt());
-                    System.out.println(somma3);
-                }
-                case 4 -> {
-                    System.out.println("Inserisci due numeri");
-                    int somma4 = moltiplicazione(in.nextInt(), in.nextInt());
-                    System.out.println(somma4);
-                }
-                case 0 -> {
-                    System.err.println("Uscita");
-                    sentinella = false;
-                }
             }
-
         }
 
     }
