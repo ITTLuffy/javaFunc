22e68cc3660c742df4f5f7af2363c4eff0e94df7
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-13 15:15:55 +0100

    Feat: project with methods

---
+++ methodsPyramid/src/App.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Inserisci un numero DISPARI");
+        int n = in.nextInt();
+        conoRovescio(n);
+    }
+
+    /**
+     * Metodo che prende come argomento un numero e stampa a video un albero capovolto
+     * @param n numero massimo
+     */
+    public static boolean conoRovescio(int n) {
+        if (n % 2 != 0) {
+            int righe = (n/2)+1; // righe che verranno stampate
+        
+            // numero di asterischi
+            int contaCar = n;
+    
+            // numero di spazi       
+            int contaSpa = 0;
+    
+            for (int i = righe; i > 0; i--) {
+                // per spazi
+                for (int j = 0; j < contaSpa; j++) {
+                    System.out.print(" ");
+                }
+                contaSpa++;
+                for (int k = 0; k < contaCar; k++) {
+                    System.out.print("*");
+                }
+                contaCar-=2;
+                System.out.println(" ");
+    
+            }    
+            return true;
+
+        } else {
+            System.err.println("Wrong");
+            return false;
+
+        }
+
+    }
+}
