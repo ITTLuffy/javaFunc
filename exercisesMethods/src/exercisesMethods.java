b942f73584f5c10fbefe5c35ffbbdb3283880411
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-14 09:27:19 +0100

    Feat: add option with while

--- exercisesMethods/src/exercisesMethods.java
+++ exercisesMethods/src/exercisesMethods.java
@@ -6,15 +6,23 @@ public class exercisesMethods {
     a video i 10 numeri successivi (fai l’esercizio sia con ciclo for che con while).
      */
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Inserisci un numero");
-        int n = in.nextInt();
-        stampa(n);
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Inserisci un numero");
+            int n = in.nextInt();
+            stampa(n);
+            in.close();
+        }
     }
 
     public static void stampa(int n) {
         for (int i = n; i <= n+10; i++) {
             System.out.println(i);
+        }
+        
+        int k = n;
+        while (k < 10+n) {
+            k++;
+            System.out.println(k);
         }
     }
 }
