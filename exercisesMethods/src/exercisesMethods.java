0468fa3fc574b616e54d190684f63566b78bb1ae
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-14 18:25:56 +0100

    Feat: add exercise 3

--- exercisesMethods/src/exercisesMethods.java
+++ exercisesMethods/src/exercisesMethods.java
@@ -1,19 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class exercisesMethods {
     /*
     Creare un metodo che prenda come argomento un numero intero e stampi 
     a video i 10 numeri successivi (fai l’esercizio sia con ciclo for che con while).
-     */
+    */
     public static void main(String[] args) throws Exception {
         try (Scanner in = new Scanner(System.in)) {
             System.out.println("Inserisci un numero");
             int n = in.nextInt();
             stampa(n);
             stampaContrario(n);
+            int[] vettore = new int[10];
+            stampaArray(vettore, n);
+            System.out.println(Arrays.toString(vettore));
             in.close();
 
         }
+
     }
 
     public static void stampa(int n) {
@@ -26,6 +31,7 @@ public class exercisesMethods {
             k++;
             System.out.println(k);
         }
+
     }
 
     public static void stampaContrario(int n) {
@@ -41,4 +47,24 @@ public class exercisesMethods {
         }
 
     }
+
+    public static void stampaArray(int[] v, int n) {
+        int j = 0;
+        for (int i = n+1; i <= n+10; i++) {
+            System.out.println(i);
+            v[j] = i;
+            j++;
+        }
+        j = 0;
+        int k = n;
+
+        while (k < 10+n) {
+            k++;
+            System.out.println(k);
+            v[j] = k;
+            j++;
+        }
+
+    }
+
 }
