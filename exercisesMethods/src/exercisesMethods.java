067c76bbc4e5faa82eb9e7704f3292d2c2223416
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-17 10:49:37 +0100

    Feat: exercise n. 7

--- exercisesMethods/src/exercisesMethods.java
+++ exercisesMethods/src/exercisesMethods.java
@@ -1,12 +1,17 @@
+
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class exercisesMethods {
+
     /*
     Creare un metodo che prenda come argomento un numero intero e stampi 
     a video i 10 numeri successivi (fai l’esercizio sia con ciclo for che con while).
-    */
+     */
     public static void main(String[] args) throws Exception {
+        Random r = new Random();
+
         try (Scanner in = new Scanner(System.in)) {
             System.out.println("Inserisci un numero");
             int n = in.nextInt();
@@ -17,17 +22,24 @@ public class exercisesMethods {
             System.out.println(Arrays.toString(vettore));
             in.close();
 
+            int[] numeri = new int[5];
+            for (int i = 0; i < numeri.length; i++) {
+                numeri[i] = r.nextInt(1, 5);
+            }
+            System.out.println(Arrays.toString(numeri));    
+            tornaArray(numeri);
+            System.out.println(Arrays.toString(numeri));
         }
 
     }
 
     public static void stampa(int n) {
         System.out.println("Numeri successivi");
-        for (int i = n+1; i <= n+10; i++) {
+        for (int i = n + 1; i <= n + 10; i++) {
             System.out.println(i);
         }
         int k = n;
-        while (k < 10+n) {
+        while (k < 10 + n) {
             k++;
             System.out.println(k);
         }
@@ -36,12 +48,12 @@ public class exercisesMethods {
 
     public static void stampaContrario(int n) {
         System.out.println("Numeri precedenti");
-        for (int i = n-1; i >= n-10; i--) {
+        for (int i = n - 1; i >= n - 10; i--) {
             System.out.println(i);
-        } 
+        }
 
         int k = n;
-        while (k > n-10) {
+        while (k > n - 10) {
             k--;
             System.out.println(k);
         }
@@ -50,7 +62,7 @@ public class exercisesMethods {
 
     public static void stampaArray(int[] v, int n) {
         int j = 0;
-        for (int i = n+1; i <= n+10; i++) {
+        for (int i = n + 1; i <= n + 10; i++) {
             System.out.println(i);
             v[j] = i;
             j++;
@@ -58,13 +70,24 @@ public class exercisesMethods {
         j = 0;
         int k = n;
 
-        while (k < 10+n) {
+        while (k < 10 + n) {
             k++;
             System.out.println(k);
             v[j] = k;
             j++;
         }
 
+    }
+
+    public static int[] tornaArray(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] % 2 == 0) {
+                v[i] = 0;
+            }
+
+        }
+
+        return v;
     }
 
 }
