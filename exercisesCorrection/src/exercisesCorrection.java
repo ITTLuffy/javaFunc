dc988ba8321318a99d137e2c59ff08167473144c
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-18 08:42:34 +0100

    Feat: add new projec

--- exercisesCorrection/src/exercisesCorrection.java
+++ exercisesCorrection/src/exercisesCorrection.java
@@ -1,8 +1,20 @@
 
 public class exercisesCorrection {
     public static void main(String[] args) throws Exception {
-        
+        int[] vet = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        occorrenze(vet, 5);
 
+
+    }
+    // contare le occorrenze di un certo elemento in un array
+    public static void occorrenze(int[] v, int valore) {
+        int occorrenze = 0;
+        for (int i : v) {
+            if (v[i] == valore) {
+                occorrenze++;
+            }
+        }
+        System.out.println("L'elemento " + valore + " è presente " + occorrenze + " volte");
     }
 
 }
