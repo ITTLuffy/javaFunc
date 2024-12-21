6a5f840908a6109025b9c68f5c08cb07ca7f1248
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2024-12-21 11:08:06 +0100

    Feat: add comments

--- holidayHomework/src/holidayHomework.java
+++ holidayHomework/src/holidayHomework.java
@@ -14,31 +14,48 @@ public class holidayHomework {
         int[] v = new int[DIM_MAX]; // vettore con dimensione massima 
 
         System.out.println("Inserisci il numero di sfere con cui vuoi giocare");
-        int dimSfere = in.nextInt();
+        int dimSfere = in.nextInt(); // dimensione array iniziale
         // riempio il vettore 
         for (int i = 0; i < dimSfere; i++) {
             riempiVettore(v, r.nextInt(0, 4));
         }
 
+        // stampo il risultato
         stampa(v);
 
         System.out.println("Inserisci la posizione della sfera da eliminare");
+        // elimino la sfera
         togliSfera(v, in.nextInt());
+        // stampo il risultato
         stampa(v);
     }
 
+    /**
+     * Riempie il vettore con un colore
+     *
+     * @param v vettore
+     * @param colore colore da inserire
+     */
     public static void riempiVettore(int[] v, int colore) {
         // 4 colori, associati ad un numero
+
+        /*
         int rosso = 0;
         int blu = 1;
         int giallo = 2;
         int verde = 3;
-
+        */
+        // inserisco il colore nel vettore 
         v[dim] = colore;
+        // incremento la posizione logica
         dim++;
-
     }
 
+    /**
+     * Stampa il vettore
+     *
+     * @param v vettore da stampare
+     */
     public static void stampa(int[] v) {
         System.out.print("[");
         for (int i = 0; i < dim; i++) {
@@ -47,15 +64,24 @@ public class holidayHomework {
         System.out.println("]");
     }
 
+    /**
+     * Elimina una sfera dal vettore
+     *
+     * @param v vettore
+     * @param pos posizione della sfera da eliminare
+     */
     public static void togliSfera(int v[], int pos) {
+        // controllo che la posizione sia valida
         if (pos < 0 || pos >= dim) {
             System.err.println("Posizione non valida");
             System.exit(0);
         }
 
+        // sposto tutti gli elementi a sinistra
         for (int i = pos + 1; i < dim; i++) {
             v[i - 1] = v[i];
         }
+        // decremento la dimensione logica
         dim--;
     }
 
