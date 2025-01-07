98deef68b59cd5c0d1ba0ede6dc73aca5b23328e
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-01-07 11:37:43 +0100

    Feat: strings

---
+++ strings/src/strings.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class strings {
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        String stringa = "";
+        int chiave = 0;
+        messaggio(stringa, chiave);
+    }
+
+    public static String messaggio(String messaggio, int k) {
+        System.out.println("Inserisci il messaggio segreto");
+        messaggio = in.next();
+        System.out.println("Inserisci la chiave");
+        k = in.nextInt();
+        // contatore posizione stringa
+        int cont = 0;
+        // contatore quante volte eseguire il ciclo
+        int contChiave = 0;
+        int riga = 1;
+        // ciclo per eseguire 
+        while (contChiave < k) {
+            // ciclo che stampa 
+            while (cont < messaggio.length()) {
+                System.out.print(messaggio.charAt(cont));
+                if (riga % 2 == 0) {
+                    cont += 2;
+                } else {
+                    cont += k + 1;
+                }
+            }
+
+            // aggiorno il contatore
+            contChiave++;
+
+
+            cont = riga-1;
+
+            cont++;
+
+            riga++;
+
+        }
+        return messaggio;
+
+    }
+
+}
